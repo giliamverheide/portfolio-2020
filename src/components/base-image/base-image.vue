0ff64fe0a8c5704fd04eef43e554f60f6ef6727e
Giliam <verheide.giliam@gmail.com>
2020-03-10 16:55:55 +0100

    Image component added

---
+++ src/components/base-image/base-image.vue
@@ -0,0 +1,44 @@
+<template>
+  <figure class="c-image o-image">
+    <div class="image__inner">
+      <picture>
+        <source :data-srcset="require(`@/assets/images/${image}?webp`)" type="image/webp" />
+        <img
+          src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+          :data-src="require(`@/assets/images/${image}`)"
+          :width="imageWidth"
+          :height="imageHeight"
+          :class="lazyload ? 'lazyload' : ''"
+        />
+      </picture>
+    </div>
+  </figure>
+</template>
+
+<script>
+/* eslint-disable import/no-dynamic-require, global-require */
+export default {
+  components: {},
+  props: {
+    image: {
+      type: String,
+      default: null,
+      required: true
+    },
+    imageWidth: {
+      type: String,
+      default: '1600'
+    },
+    imageHeight: {
+      type: String,
+      default: '1000'
+    },
+    lazyload: {
+      type: Boolean,
+      default: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
