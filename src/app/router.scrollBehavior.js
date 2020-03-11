06c2589535ea09ff81dbc259484e5850d58d965b
Giliam <verheide.giliam@gmail.com>
2020-03-11 23:58:03 +0100

    scrollBehavior changes

--- src/app/router.scrollBehavior.js
+++ src/app/router.scrollBehavior.js
@@ -1,53 +1,62 @@
+/**
+ * Fixes scroll behavior
+ * https://levelup.gitconnected.com/nuxt-js-custom-scrollbehavior-fired-after-page-loaded-cd94fd6ddd12
+ */
+
 if (process.client) {
   if ('scrollRestoration' in window.history) {
-    window.history.scrollRestoration = 'manual'
+    window.history.scrollRestoration = 'manual';
     // reset scrollRestoration to auto when leaving page, allowing page reload
     // and back-navigation from other pages to use the browser to restore the
     // scrolling position.
     window.addEventListener('beforeunload', () => {
-      window.history.scrollRestoration = 'auto'
-    })
+      window.history.scrollRestoration = 'auto';
+    });
     // Setting scrollRestoration to manual again when returning to this page.
     window.addEventListener('load', () => {
-      window.history.scrollRestoration = 'manual'
-    })
+      window.history.scrollRestoration = 'manual';
+    });
   }
 }
 
-export default function (to, from, savedPosition) {
-  let position;
-  //Your Code Starts Here
-  position = savedPosition || { x: 0, y: 0 };
+function scrollBehavior(to, from, savedPosition) {
+  let position = savedPosition || { x: 0, y: 0 };
+
+  // Always scroll to the top on project pages
   if (to.name === 'projects-slug') {
     position = { x: 0, y: 0 };
   }
-  //Your Code Ends Here
-  const nuxt = window.<%= globals.nuxt %>;
+
+  // $nuxt.scrollPosition = position;
+
   // triggerScroll is only fired when a new component is loaded
   if (to.path === from.path && to.hash !== from.hash) {
-    nuxt.$nextTick(() => nuxt.$emit('triggerScroll'))
+    $nuxt.$nextTick(() => $nuxt.$emit('triggerScroll'));
   }
-  return new Promise((resolve) => {
+
+  return new Promise(resolve => {
     // wait for the out transition to complete (if necessary)
-    nuxt.$once('triggerScroll', () => {
+    $nuxt.$once('triggerScroll', () => {
       // coords will be used if no selector is provided,
       // or if the selector didn't match any element.
       if (to.hash) {
-        let hash = to.hash
+        let { hash } = to;
         // CSS.escape() is not supported with IE and Edge.
         if (typeof window.CSS !== 'undefined' && typeof window.CSS.escape !== 'undefined') {
-          hash = '#' + window.CSS.escape(hash.substr(1))
+          hash = `#${window.CSS.escape(hash.substr(1))}`;
         }
         try {
           if (document.querySelector(hash)) {
             // scroll to anchor by returning the selector
-            position = { selector: hash }
+            position = { selector: hash };
           }
         } catch (e) {
-          console.warn('Failed to save scroll position. Please add CSS.escape() polyfill.')
+          console.warn('Failed to save scroll position. Please add CSS.escape() polyfill.');
         }
       }
-      resolve(position)
-    })
-  })
+      resolve(position);
+    });
+  });
 }
+
+export default scrollBehavior;
